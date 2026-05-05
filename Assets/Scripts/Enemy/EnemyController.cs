fae1f48b856979c1077ab37004d22770f3310ef8
Alp Doruk <alpotheo@gmail.com>
2026-05-05 10:58:01 +0300

    feat: add EnemyController skeleton with NavMesh and behavior switching

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,11 +1,55 @@
 using UnityEngine;
+using UnityEngine.AI;
 
-/// <summary>
-/// STUB - Alp'in gerçek EnemyController'ý gelene kadar derleme hatasý vermemek için.
-/// Alp'in kodu merge edilince bu dosya silinecek.
-/// Assets/Scripts/Enemy/EnemyController.cs
-/// </summary>
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyController : MonoBehaviour
 {
-    public void SetBlinded(bool blinded, float duration) { }
+    [Header("Patrol")]
+    [SerializeField] private Transform[] _patrolWaypoints;
+
+    [Header("Detection")]
+    [SerializeField] private float _sightRange = 15f;
+    [SerializeField] private float _attackRange = 2f;
+    [SerializeField] private float _noiseDetectionRadius = 8f;
+    [SerializeField] private LayerMask _playerLayer;
+
+    public NavMeshAgent Agent { get; private set; }
+    public Transform[] PatrolWaypoints => _patrolWaypoints;
+    public Transform CurrentTarget { get; set; }
+    public int CurrentWaypointIndex { get; set; }
+    public bool HeardNoise { get; set; }
+
+    private IEnemyBehavior _current;
+
+    private void Awake()
+    {
+        Agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        SwitchBehavior(new PatrolBehavior());
+    }
+
+    private void Update()
+    {
+        _current?.Tick(this);
+    }
+
+    private void OnDestroy()
+    {
+        _current?.Exit(this);
+    }
+
+    public void SwitchBehavior(IEnemyBehavior next)
+    {
+        _current?.Exit(this);
+        _current = next;
+        _current.Enter(this);
+    }
+
+    public void SetBlinded(bool blinded, float duration)
+    {
+        // TODO Sprint 2: FleeBehavior entegrasyonu
+    }
 }
