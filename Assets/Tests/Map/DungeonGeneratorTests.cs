35c482494d79e7549b75097cea66690db94f0c96
Metin Enes Ufuk <metinenes76@gmail.com>
2026-05-11 02:39:02 +0300

    test setup

---
+++ Assets/Tests/Map/DungeonGeneratorTests.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+using UnityEngine;
+
+[TestFixture]
+public class DungeonGeneratorTests
+{
+    [Test]
+    public void Assembly_IsSetUp()
+    {
+        Assert.Pass();
+    }
+}
