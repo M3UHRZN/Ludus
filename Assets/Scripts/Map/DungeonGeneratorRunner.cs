d24658feeb2d020b792897eba15eacc3c52ee1a2
Metin Enes Ufuk <metinenes76@gmail.com>
2026-05-23 10:37:34 +0300

    seed sync and lobby to map implement

--- Assets/Scripts/Map/DungeonGeneratorRunner.cs
+++ Assets/Scripts/Map/DungeonGeneratorRunner.cs
@@ -1,10 +1,48 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class DungeonGeneratorRunner : MonoBehaviour
+[RequireComponent(typeof(NetworkObject))]
+public class DungeonGeneratorRunner : NetworkBehaviour
 {
     [SerializeField] private DungeonGeneratorSO _config;
 
-    private void Start() => GenerateAndVisualize();
+    private readonly NetworkVariable<int> _dungeonSeed = new NetworkVariable<int>(
+        0,
+        NetworkVariableReadPermission.Everyone,
+        NetworkVariableWritePermission.Server
+    );
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            int seedToUse = _config.useRandomSeed ? System.Environment.TickCount : _config.seed;
+            _dungeonSeed.Value = seedToUse;
+            GenerateAndVisualize(seedToUse);
+        }
+        else
+        {
+            if (_dungeonSeed.Value != 0)
+            {
+                GenerateAndVisualize(_dungeonSeed.Value);
+            }
+            else
+            {
+                _dungeonSeed.OnValueChanged += OnSeedChanged;
+            }
+        }
+    }
+
+    private void OnSeedChanged(int previousValue, int newValue)
+    {
+        _dungeonSeed.OnValueChanged -= OnSeedChanged;
+        GenerateAndVisualize(newValue);
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        _dungeonSeed.OnValueChanged -= OnSeedChanged;
+    }
 
     [ContextMenu("Generate Dungeon")]
     public void GenerateAndVisualize()
