97f8cd4c890ab44512901f84a058a92041cb3667
Metin Enes Ufuk <metinenes76@gmail.com>
2026-05-31 16:09:36 +0300

    Update ExtractionUIController.cs

--- Assets/Scripts/UI/ExtractionUIController.cs
+++ Assets/Scripts/UI/ExtractionUIController.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using Unity.Netcode; // AĞ KÜTÜPHANESİ EKLENDİ
 
 public class ExtractionUIController : MonoBehaviour
 {
@@ -16,105 +15,60 @@ public class ExtractionUIController : MonoBehaviour
     [Header("Kota Barı")]
     public Image quotaFillImage;
 
-    // Öldüğümüzde ekrandan silinmesini istediğimiz şeyler (Timer, Stamina vb.)
     [Header("Gizlenecek HUD Parçaları")]
     public GameObject[] hudElementsToHide;
 
     private void Awake()
     {
-        // GameSessionManager'ın fırlattığı sinyali dinliyoruz!
-        GameEventBus.Subscribe<SessionEndedEvent>(OnSessionEnded);
-        GameEventBus.Subscribe<PlayerDiedEvent>(OnPlayerDied);
+        GameEventBus.Subscribe<RunResultEvent>(OnRunResult);
     }
 
     private void OnDestroy()
     {
-        GameEventBus.Unsubscribe<SessionEndedEvent>(OnSessionEnded);
-        GameEventBus.Unsubscribe<PlayerDiedEvent>(OnPlayerDied);
+        GameEventBus.Unsubscribe<RunResultEvent>(OnRunResult);
     }
 
     private void Start()
     {
-        extractionPanel.SetActive(false);
+        if (extractionPanel != null) extractionPanel.SetActive(false);
     }
 
-    // SÜRE/OYUN BİTTİĞİNDE ÇALIŞIR
-    private void OnSessionEnded(SessionEndedEvent evt)
+    private void OnRunResult(RunResultEvent evt)
     {
-        bool isSuccess = (evt.Reason == SessionEndReason.Escaped);
+        bool isSuccess = evt.Reason == SessionEndReason.Escaped || evt.Reason == SessionEndReason.TimeUp;
+        bool isWipe    = evt.Reason == SessionEndReason.AllDead;
 
-        // === Parayı ExtractionManager dan çekiyoruz! ===
-        int finalCredits = 0;
-        if (ExtractionManager.Instance != null)
-        {
-            finalCredits = ExtractionManager.Instance.TotalCredits.Value;
-        }
-
-        ShowExtractionScreen(isSuccess, finalCredits, 0, 0f);
+        ShowExtractionScreen(isSuccess && !isWipe, evt.Net, evt.Penalty, isWipe);
     }
 
-    // BİZ ÖLDÜĞÜMÜZDE ÇALIŞIR
-    private void OnPlayerDied(PlayerDiedEvent evt)
+    public void ShowExtractionScreen(bool isSuccess, int netCredits, int penaltyAmount, bool isWipe)
     {
-        if (NetworkManager.Singleton != null && evt.PlayerId == (int)NetworkManager.Singleton.LocalClientId)
-        {
-            // === Parayı ExtractionManager dan çekiyoruz! ===
-            int currentCredits = 0;
-            if (ExtractionManager.Instance != null)
-            {
-                currentCredits = ExtractionManager.Instance.TotalCredits.Value;
-            }
-
-            ShowExtractionScreen(false, currentCredits, 50, 0f);
-        }
-    }
-
-    public void ShowExtractionScreen(bool isSuccess, int collectedCredits, int penaltyAmount, float quotaFillAmount)
-    {
-        extractionPanel.SetActive(true);
-        // Oyuncu butona tıklayabilsin diye farenin kilidini aç ve görünür yap!
+        if (extractionPanel != null) extractionPanel.SetActive(true);
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
 
-        // Arkadaki Timer, Stamina, Envanter gibi şeyleri gizliyoruz
         if (hudElementsToHide != null)
-        {
             foreach (var element in hudElementsToHide)
-            {
                 if (element != null) element.SetActive(false);
+
+        if (titleText != null)
+        {
+            if (isWipe)        { titleText.text = "TEAM WIPED";      titleText.color = Color.red; }
+            else if (isSuccess){ titleText.text = "MISSION COMPLETE"; titleText.color = Color.white; }
+            else               { titleText.text = "MISSION FAILED";   titleText.color = Color.red; }
+        }
+
+        if (creditsText != null)
+            creditsText.text = "CREDITS EARNED: " + netCredits;
+
+        if (penaltyText != null)
+        {
+            if (penaltyAmount > 0)
+            {
+                penaltyText.text = "PENALTY: -" + penaltyAmount + " CREDITS";
+                penaltyText.gameObject.SetActive(true);
             }
+            else penaltyText.gameObject.SetActive(false);
         }
-
-        if (isSuccess)
-        {
-            titleText.text = "MISSION SUCCESS";
-            titleText.color = Color.white;
-        }
-        else
-        {
-            titleText.text = "MISSION FAILED";
-            titleText.color = Color.red;
-        }
-
-        creditsText.text = "COLLECTED CREDITS: " + collectedCredits;
-
-        if (penaltyAmount > 0)
-        {
-            penaltyText.text = "PENALTY: -" + penaltyAmount + " CREDITS";
-            penaltyText.gameObject.SetActive(true);
-        }
-        else
-        {
-            penaltyText.gameObject.SetActive(false);
-        }
-
-        if (quotaFillImage != null)
-            quotaFillImage.fillAmount = quotaFillAmount;
-    }
-
-    public void ReturnToShip()
-    {
-        extractionPanel.SetActive(false);
-        // SceneManager.LoadScene("LobbyScene"); 
     }
 }
